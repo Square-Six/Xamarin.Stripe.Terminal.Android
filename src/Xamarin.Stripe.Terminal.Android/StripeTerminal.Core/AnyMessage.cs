7b9cf200b4e8a1a531b2e393196e40e2a92bd745
Chris Oswald <christopher_mark_oswald@hotmail.com>
2021-11-29 08:07:59 -0800

    Stripe Terminal 2.0 compile working

---
+++ src/Xamarin.Stripe.Terminal.Android/StripeTerminal.Core/AnyMessage.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//namespace Com.Squareup.Wire
+//{
+//    public partial class AnyMessage
+//    {
+//        public override Java.Lang.Object NewBuilder()
+//        {
+//            return null;
+//        }
+//    }
+//}
---
+++ src/Xamarin.Stripe.Terminal.Android/Xamarin.SquareUp.Wire/AnyMessage.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//namespace Com.Squareup.Wire
+//{
+//    public partial class AnyMessage : global::Com.Squareup.Wire.Message
+//    {
+//        public override Java.Lang.Object NewBuilder()
+//        {
+//            return null;
+//        }
+//    }
+//}
