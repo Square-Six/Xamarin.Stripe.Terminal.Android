23f17d647f4d3ee96303ff45ad3d5cc5017ad048
Chris Oswald <christopher_mark_oswald@hotmail.com>
2021-12-04 21:51:46 -0600

    Fix Meta Data

---
+++ src/Xamarin.Stripe.Terminal.Android/StripeTerminal.Internal.Common/NullOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Stripe.Stripeterminal.Internal.Common
+{
+    partial class NullOperation
+    {
+        public global::Java.Lang.Object Execute()
+        {
+            return null;
+        }
+
+    }
+}
---
+++ src/Xamarin.Stripe.Terminal.Android/StripeTerminal.Internal.Common/UpdatePackage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Stripe.Readerupdate
+{
+    public partial class UpdatePackage
+    {
+
+        public unsafe global::System.Collections.IList SubList(int fromIndex, int toIndex)
+        {
+            return SubListWrap(fromIndex, toIndex) as System.Collections.IList;
+        }
+
+            
+    }
+}
