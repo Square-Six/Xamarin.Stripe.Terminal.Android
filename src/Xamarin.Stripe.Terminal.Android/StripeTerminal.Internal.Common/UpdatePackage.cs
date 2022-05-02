9d8923efb1fec21fa5c9b26296d3003bcbf22891
Chris Oswald <christopher_mark_oswald@hotmail.com>
2022-05-02 08:37:37 -0700

    Fix Compile Errors on 2.9

--- src/Xamarin.Stripe.Terminal.Android/StripeTerminal.Internal.Common/UpdatePackage.cs
+++ src/Xamarin.Stripe.Terminal.Android/StripeTerminal.Internal.Common/UpdatePackage.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Com.Stripe.Readerupdate
 {
-    public sealed partial class UpdatePackage
+    public partial class UpdatePackage
     {
 
         public unsafe global::System.Collections.IList SubList(int fromIndex, int toIndex)
@@ -15,13 +15,177 @@ namespace Com.Stripe.Readerupdate
     }
 }
 
-//namespace Com.Stripe.Core.Paymentcollection
-//{
-//    public sealed partial class TippingHandler_Factory
-//    {
-//        public unsafe global::Java.Lang.Object Get()
-//        {
-//            return this;
-//        }
-//    }
-//}
+namespace Com.Stripe.Core.Paymentcollection
+{
+
+    public partial class TippingHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class RemoveCardHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class ProcessingHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class PrepareToCollectAgainHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class PinEntryHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class PaymentCollectionStateMachine_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class OnlineConfirmationHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class ApplicationSelectionHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class ApplicationSelectionCommitHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class AccountSelectionHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class CancelHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class CollectionCompleteHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class CollectionHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class CollectionFallbackHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class DisplayCartPostCollectionHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class EmptyHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+
+
+    public partial class DisplayCartPreCollectionHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class FinishedHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class LanguageSelectionHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class OnlineAuthorizationHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+    public partial class OnlineAuthorizationMagStripeHandler_Factory
+    {
+        public global::Java.Lang.Object Get()
+        {
+            return this;
+        }
+    }
+
+}
