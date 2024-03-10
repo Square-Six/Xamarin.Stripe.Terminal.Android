55753f6befd593b354e082f927217397aa3734c2
Chris Oswald <christopher_mark_oswald@hotmail.com>
2024-03-10 14:54:18 -0500

    Updates

--- src/Xamarin.Stripe.Terminal.Android/Xamarin.SquareUp.Moshi/Dispatcher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Square.OkHttp3
-{
-    partial class Dispatcher
-    {
-        public void SetIdleCallback(Java.Lang.IRunnable idleCallback) =>
-            IdleCallback = idleCallback;
-    }
-}
